ff6a1e4899b5d6ff74f44c687d1233874d2b44fb
HL Tool <shoexproff@gmail.com>
2025-03-13 05:14:38 +0100

    tool.py

--- tool.py
+++ tool.py
@@ -68,15 +68,15 @@ def load_player_data(cpm):
         data = response.get('data')
         if 'floats' in data and 'localID' in data and 'money' in data and 'coin' in data:
         
-            print(Colorate.Horizontal(Colors.red, '==========[ PLAYER DETAILS ]=========='))
+            print(Colorate.Horizontal(Colors.red_to_white, '==========[ PLAYER DETAILS ]=========='))
             
-            print(Colorate.Horizontal(Colors.white, f'Name   : {(data.get("Name") if "Name" in data else "UNDEFINED")}.'))
+            print(Colorate.Horizontal(Colors.red_to_white, f'Name   : {(data.get("Name") if "Name" in data else "UNDEFINED")}.'))
                 
-            print(Colorate.Horizontal(Colors.rainbow, f'LocalID: {data.get("localID")}.'))
+            print(Colorate.Horizontal(Colors.red_to_white, f'LocalID: {data.get("localID")}.'))
             
-            print(Colorate.Horizontal(Colors.rainbow, f'Money  : {data.get("money")}.'))
+            print(Colorate.Horizontal(Colors.red_to_white, f'Money  : {data.get("money")}.'))
             
-            print(Colorate.Horizontal(Colors.rainbow, f'Coins  : {data.get("coin")}.'))
+            print(Colorate.Horizontal(Colors.red_to_white, f'Coins  : {data.get("coin")}.'))
             
         else:
             print(Colorate.Horizontal(Colors.rainbow, '! ERROR: new accounts most be signed-in to the game at least once !.'))
